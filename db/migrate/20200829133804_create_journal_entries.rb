3516be5e26c274e2e3393964e5f3735ac5859904
JeffEckert <bromodo@gmail.com>
2020-08-29 11:00:19 -0400

    changed date_id in journal entry migration to belongs_to :calender_date and ran migrate

--- db/migrate/20200829133804_create_journal_entries.rb
+++ db/migrate/20200829133804_create_journal_entries.rb
@@ -4,7 +4,7 @@ class CreateJournalEntries < ActiveRecord::Migration[6.0]
       t.string :meal
       t.string :description
       t.string :image_url
-      t.integer :date_id
+      t.belongs_to :calendar_dates
       t.integer :calorie_count
       
       t.timestamps
