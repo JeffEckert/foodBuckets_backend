271b6bcb74b90fa6391138e33bc81d109ef99456
JeffEckert <bromodo@gmail.com>
2020-08-29 09:50:39 -0400

    added seed data for journalEntry

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+JournalEntry.create(meal: "Breakfast", description: "Sandwich thin, egg, canadian bacon, chedder cheese", 
+calorie_count: 310, date_id: date.id)
