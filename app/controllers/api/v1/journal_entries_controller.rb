c7b921fa4e53a2a3a75aa0cf173c98fcfe4a3988
JeffEckert <bromodo@gmail.com>
2020-08-30 19:34:39 -0400

    added create to journal entry controller as well as strong params

--- app/controllers/api/v1/journal_entries_controller.rb
+++ app/controllers/api/v1/journal_entries_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::JournalEntriesController < ApplicationController
 
     def index
-        @journal_entries = JournalEntry.all 
-        render json: @journal_entries
+        journal_entries = JournalEntry.all 
+        render json: journal_entries
+    end
+
+    def create
+        journal_entries = JournalEntry.new(journal_entry_params)
+    end
+
+    private
+
+    def journal_entry_params
+        params.require(:Journal_entry).permit(:meal, :description, :image_url, :calorie_count, :calendar_date_id)
     end
 end
