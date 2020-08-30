d93e6c58db825d48db9fe59d4f5bac95ab16c0e3
JeffEckert <bromodo@gmail.com>
2020-08-30 19:08:02 -0400

    added controller for journal_entries and built routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # namespace api do
-  #   namespace v1 do
-  #     resources :journal_entries, only: [:index]
-  #   end
-  # end
+  namespace :api do
+    namespace :v1 do
+      resources :journal_entries, only: [:index]
+    end
+  end
 end
