19d2c636baef776714d9c05b1a118cc350aad136
JeffEckert <bromodo@gmail.com>
2020-09-01 19:31:51 -0400

    Added Calendar_date index method to Controller

--- app/controllers/api/v1/calendar_dates_controller.rb
+++ app/controllers/api/v1/calendar_dates_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::CalendarDatesController < ApplicationController
+
+    def index
+        calendar_dates = CalendarDate.all 
+        render json: CalendarDateSerializer.new(calendar_dates)
+    end
+
+    def create 
+
+    end
 end
