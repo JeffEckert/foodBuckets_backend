ffa407f9d121b298a406e102f419f97afa553e50
JeffEckert <bromodo@gmail.com>
2020-09-01 19:35:52 -0400

    added calendar_date create method to controller and added private Strong params helper

--- app/controllers/api/v1/calendar_dates_controller.rb
+++ app/controllers/api/v1/calendar_dates_controller.rb
@@ -6,6 +6,18 @@ class Api::V1::CalendarDatesController < ApplicationController
     end
 
     def create 
+        calendar_date = CalendarDate.new(calendar_date_params)
+        if calendar_date.save
+            render json: calendar_date, status: :accepted 
+        else
+            render json: {errors: calendar_date.errors.full_messages}, status: :uprocessable_entity
+        end
+    end
 
+
+    private
+
+    def calendar_date_params
+        params.require(:Calendar_date).permit(:date)
     end
 end
