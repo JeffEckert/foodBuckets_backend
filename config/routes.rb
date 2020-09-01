7b67dbab1be00f63ddcbecd146cc83e381fd84fb
JeffEckert <bromodo@gmail.com>
2020-09-01 19:23:53 -0400

    added routes for Calendar_entries model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :journal_entries, only: [:index, :create]
+      resources :calendar_dates, only: [:index, :create]
     end
   end
 end
