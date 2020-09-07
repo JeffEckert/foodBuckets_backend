bfe012e8314f8cbe2c8643c5ee504757bb948e71
JeffEckert <bromodo@gmail.com>
2020-09-07 11:08:53 -0400

    pushing up changes to serializer to fix issue with post fetch

--- app/serializers/journal_entry_serializer.rb
+++ app/serializers/journal_entry_serializer.rb
@@ -1,4 +1,4 @@
 class JournalEntrySerializer
   include FastJsonapi::ObjectSerializer
-  attributes :meal, :description, :image_url, :calorie_count, :category_id, :date
+  attributes :meal, :description, :image_url, :calorie_count, :category_id, :date, :category
 end
